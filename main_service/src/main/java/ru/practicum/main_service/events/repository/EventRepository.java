af0affe4dfd107695cc49c29139d9f153757d76d
Gudisa <gudisa.otyrba@yandex.ru>
2023-05-22 18:46:14 +0300

    without stat views style fix

--- main_service/src/main/java/ru/practicum/main_service/events/repository/EventRepository.java
+++ main_service/src/main/java/ru/practicum/main_service/events/repository/EventRepository.java
@@ -1,7 +1,6 @@
 package ru.practicum.main_service.events.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.practicum.main_service.events.models.Event;
 
