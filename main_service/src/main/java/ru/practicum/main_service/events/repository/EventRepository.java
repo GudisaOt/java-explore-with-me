e1e7f1221f5097110fc524b6b109804396e07c27
Gudisa <gudisa.otyrba@yandex.ru>
2023-05-24 22:20:05 +0300

    fix code

--- main_service/src/main/java/ru/practicum/main_service/events/repository/EventRepository.java
+++ main_service/src/main/java/ru/practicum/main_service/events/repository/EventRepository.java
@@ -2,9 +2,7 @@ package ru.practicum.main_service.events.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.practicum.main_service.events.models.Event;
 
