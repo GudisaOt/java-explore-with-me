c2a0fb368bed8daa2d237bb1a62d6fed92ae77de
Gudisa <gudisa.otyrba@yandex.ru>
2023-05-27 21:18:22 +0300

    code style fix 2

--- main_service/src/main/java/ru/practicum/main_service/comments/repository/CommentRepository.java
+++ main_service/src/main/java/ru/practicum/main_service/comments/repository/CommentRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findAllByAuthorId(Long id);
-    
+
     List<Comment> findAllByEventId(Long id);
 }
