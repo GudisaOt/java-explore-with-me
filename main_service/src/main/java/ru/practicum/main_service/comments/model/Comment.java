eb589d57dfcdcd294a917ac53a5cf139d57c13d3
Gudisa <gudisa.otyrba@yandex.ru>
2023-05-27 21:27:02 +0300

    fix custom test

--- main_service/src/main/java/ru/practicum/main_service/comments/model/Comment.java
+++ main_service/src/main/java/ru/practicum/main_service/comments/model/Comment.java
@@ -20,20 +20,26 @@ public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     private String text;
+
     @Column(nullable = false)
     @Builder.Default
     private int positive = 0;
+
     @Column(nullable = false)
     @Builder.Default
     private int negative = 0;
+
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "author_id", referencedColumnName = "id")
     private User author;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "event_id", referencedColumnName = "id")
     private Event event;
+    
     @Column(nullable = false)
     private LocalDateTime created;
 }
